8b8690a6773f1d125cddea459cff2f8f1b7fd52f
Yevhen <59817721+MEGATREX4@users.noreply.github.com>
2025-10-15 21:59:52 +0300

    update mod to compatibility with 1.21.9+1.21.10

--- src/client/java/com/megatrex4/KeyBindings.java
+++ src/client/java/com/megatrex4/KeyBindings.java
@@ -3,18 +3,24 @@ package com.megatrex4;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.option.KeyBinding.Category;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
+import net.minecraft.util.Identifier;
+
+import static com.megatrex4.RandomBlockPlacement.MOD_ID;
 
 public class KeyBindings {
     public static KeyBinding randomPlaceKey;
 
     public static void registerKeyBindings() {
+        Category category = Category.create(Identifier.of(MOD_ID, "category"));
+
         randomPlaceKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.randomblockplacement.toggle", // Updated to match usage
+                "key.randomblockplacement.toggle",
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_R,
-                "category.randomblockplacement"
+                category
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
